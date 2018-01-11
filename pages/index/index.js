cacbeb7205f236be4332f169d0a07f6fd1eab86b
bianhuikang <879857854@qq.com>
2018-01-11 11:12:14 +0800

    11

---
+++ pages/index/index.js
@@ -0,0 +1,13 @@
+
+Page({
+  data: {
+    stars: [0, 1, 2, 3, 4,5],//星星数量
+    normalSrc: '../../images/normal.png',
+    selectedSrc: '../../images/selected.png',
+    halfSrc:'../../images/half.png',
+    key: 3,//评分
+  },
+  onLoad: function () {
+  }
+ 
+})
